6ee45168863ff7b3aee3fbd563d6b036f6abb922
katonCODE <danielhuang47@gmail.com>
2025-08-05 23:53:12 +1200

    Basic code written to familiarise myself with c#

--- Program.cs
+++ Program.cs
@@ -1,10 +1,35 @@
-﻿namespace CS_Basics
+﻿using System.Net.WebSockets;
+
+namespace CS_Basics
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Yurrr");
+            Console.WriteLine("c# basics");
+
+            TestClass ts = new TestClass(6, 7);
+            Console.WriteLine($"num 1 = {ts.num1}\n");
+            Console.WriteLine($"num 2 = {ts.num2}\n");
+            Console.WriteLine(ts.TestFunction());
         }
+
+        class TestClass
+        {
+            public int num1 { get; set; }
+            public int num2 { get; set; }
+            public TestClass(int n1, int n2)
+            {
+                this.num1 = n1;
+                this.num2 = n2;
+            }
+            public int TestFunction()
+            {
+                return this.num1 + this.num2;
+            }
+
+
+        }
+
     }
 }
